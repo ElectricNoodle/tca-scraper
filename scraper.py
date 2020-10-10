1e90439a2f2bfbb51a06e9cf69fa456f4da3c77e
proce55ing <mail@proce55ing.com>
2020-10-10 20:46:05 +0000

    Dockerized the scraper daemon, removed file storage

--- scraper.py
+++ scraper.py
@@ -1,32 +1,17 @@
-import time, threading, tstables,tables,urllib.request,re,ast,datetime
-import pandas as pd
+import time
+import urllib.request
+import re
+import threading
+import ast
+import datetime
 from bs4 import BeautifulSoup
-
-
-class TCAData(tables.IsDescription):
-    code = tables.StringCol(64)
-    name = tables.StringCol(64)
-    capacity = tables.IntCol(pos=2)
-    count = tables.IntCol(pos=3)
-    timestamp = tables.Float64Col(pos=4)
-
-
-
-
-
 
 scrape_url = "https://portal.rockgympro.com/portal/public/d0f355e237dda999f3112d94d3c762c7/occupancy"
 SCRAPE_INTERVAL = 5
-scrape_regex = re.compile('var data = (.*?);', re.DOTALL | re.MULTILINE )
+scrape_regex = re.compile('var data = (.*?);', re.DOTALL | re.MULTILINE)
 
-f = tables.open_file('tca.h5','a')
-
-# Create a new time series
-ts = f.create_ts('/','BPI',TCAData)
 
 def scrape_data():
-
-
     fp = urllib.request.urlopen(scrape_url)
     mybytes = fp.read()
 
@@ -35,32 +20,28 @@ def scrape_data():
 
     soup = BeautifulSoup(html, 'html.parser')
     scripts = soup.find_all('script')
-   
+
     data = scrape_regex.search(str(scripts[2].string))
     print(scripts[2])
 
     if data:
         save_data(data.group(1))
 
-
     threading.Timer(SCRAPE_INTERVAL, scrape_data).start()
 
+
 def save_data(raw_data):
-  
-    data= ast.literal_eval(raw_data)
+
+    data = ast.literal_eval(raw_data)
     time = datetime.datetime.now()
 
     data = {'GymCode':  ['BRI', 'UNC', 'GLA', 'PST'],
-        'GymName': ['The Mothership', 'The Church', 'The Newsroom', 'The Prop Store'],
-        'Capacity': [data['BRI']['capacity'], data['UNC']['capacity'], data['GLA']['capacity'], data['PST']['capacity']],
-        'Count': [data['BRI']['count'], data['UNC']['count'], data['GLA']['count'], data['PST']['count']],
-        'Time':[time,time,time,time]
-    }
+            'GymName': ['The Mothership', 'The Church', 'The Newsroom', 'The Prop Store'],
+            'Capacity': [data['BRI']['capacity'], data['UNC']['capacity'], data['GLA']['capacity'], data['PST']['capacity']],
+            'Count': [data['BRI']['count'], data['UNC']['count'], data['GLA']['count'], data['PST']['count']],
+            'Time': [time, time, time, time]
+            }
 
     print(data)
-    
-    df = pd.DataFrame (data, columns = ['GymCode', 'GymName', 'Capacity', 'Count','Time'])
-    ts.append(df)
-    print (df)
 
 scrape_data()
