ee224c856b6365dfc00cf0f6331610373f8bd56c
proce55ing <mail@proce55ing.com>
2020-10-10 21:33:47 +0000

    Scraper now stores the metrics in InfluxDB

--- scraper.py
+++ scraper.py
@@ -5,11 +5,17 @@ import threading
 import ast
 import datetime
 from bs4 import BeautifulSoup
+from influxdb import InfluxDBClient
+import os
+
+influx_db_user = os.environ['INFLUXDB_USER']
+influx_db_user = os.environ['INFLUXDB_PASSWORD']
 
 scrape_url = "https://portal.rockgympro.com/portal/public/d0f355e237dda999f3112d94d3c762c7/occupancy"
 SCRAPE_INTERVAL = 5
 scrape_regex = re.compile('var data = (.*?);', re.DOTALL | re.MULTILINE)
 
+client = InfluxDBClient('influxdb', 8086, 'admin', 'admin', 'tca')
 
 def scrape_data():
     fp = urllib.request.urlopen(scrape_url)
@@ -35,13 +41,22 @@ def save_data(raw_data):
     data = ast.literal_eval(raw_data)
     time = datetime.datetime.now()
 
-    data = {'GymCode':  ['BRI', 'UNC', 'GLA', 'PST'],
-            'GymName': ['The Mothership', 'The Church', 'The Newsroom', 'The Prop Store'],
-            'Capacity': [data['BRI']['capacity'], data['UNC']['capacity'], data['GLA']['capacity'], data['PST']['capacity']],
-            'Count': [data['BRI']['count'], data['UNC']['count'], data['GLA']['count'], data['PST']['count']],
-            'Time': [time, time, time, time]
-            }
+    gyms = {'The Mothership':'BRI', 'The Church': 'UNC', 'The Newsroom': 'GLA', 'The Prop Store' : 'PST'};
 
-    print(data)
-
+    for gym in gyms.keys():
+        gym_code = gyms[gym]
+        json_body = [
+                {
+                    "measurement": 'occupancy',
+                    "tags": {
+                        "gym_code": gym_code,
+                    },
+                    "time": time.isoformat(),
+                    "fields": {
+                        "value": data[gym_code]['capacity']
+                    }
+                }
+            ]
+        client.write_points(json_body)
+    
 scrape_data()
