2a7bfc8c7c3a719418c2703e3ba8fc09e63d986a
proce55ing <mail@proce55ing.com>
2020-11-11 21:12:29 +0000

    Added some exception catching

--- api/app.py
+++ api/app.py
@@ -7,10 +7,12 @@ import os
 influx_db_user = os.environ['INFLUXDB_USER']
 influx_db_pass = os.environ['INFLUXDB_PASSWORD']
 
-client = InfluxDBClient('influxdb', 8086, influx_db_user, influx_db_pass, 'tca')
+client = InfluxDBClient(
+    'influxdb', 8086, influx_db_user, influx_db_pass, 'tca')
 
 app = Flask(__name__)
 CORS(app)
+
 
 @app.route('/', methods=['GET'])
 @app.route('/<gym_code>', methods=['GET'])
@@ -20,13 +22,15 @@ def values(gym_code="all"):
 
     if startTime is not None:
         try:
-            startTime = datetime.strptime(startTime, "%Y-%m-%dT%H:%M:%S%z").isoformat()
+            startTime = datetime.strptime(
+                startTime, "%Y-%m-%dT%H:%M:%S%z").isoformat()
         except ValueError:
-            startTime = (datetime.now() - timedelta(hours = 1)).isoformat()
-    
+            startTime = (datetime.now() - timedelta(hours=1)).isoformat()
+
     if endTime is not None:
         try:
-            endTime = datetime.strptime(endTime, "%Y-%m-%dT%H:%M:%S%z").isoformat()
+            endTime = datetime.strptime(
+                endTime, "%Y-%m-%dT%H:%M:%S%z").isoformat()
         except ValueError:
             endTime = datetime.now().isoformat()
 
@@ -37,8 +41,10 @@ def values(gym_code="all"):
         if gym_code in gym_codes:
             whereStatement = ' AND ("gym_code"=\'' + gym_code + '\')'
 
-    results = client.query('SELECT mean("value") AS "mean_value" FROM "tca"."autogen"."occupancy" WHERE time >= \'' + startTime + '\' AND time <= \'' + endTime + '\'' + whereStatement + ' GROUP BY time(5m), "gym_code"')
+    results = client.query('SELECT mean("value") AS "mean_value" FROM "tca"."autogen"."occupancy" WHERE time >= \'' +
+                           startTime + '\' AND time <= \'' + endTime + '\'' + whereStatement + ' GROUP BY time(5m), "gym_code"')
     return jsonify(results.raw)
+
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
--- scraper/scraper.py
+++ scraper/scraper.py
@@ -13,36 +13,42 @@ influx_db_pass = os.environ['INFLUXDB_PASSWORD']
 
 try:
     scrape_url = os.environ['SCRAPE_URL'] or "https://portal.rockgympro.com/portal/public/d0f355e237dda999f3112d94d3c762c7/occupancy"
-    SCRAPE_INTERVAL =  int(os.environ['SCRAPE_INTERVAL']) or 300
-except  KeyError:
+    SCRAPE_INTERVAL = int(os.environ['SCRAPE_INTERVAL']) or 300
+except KeyError:
     pass
 
 scrape_regex = re.compile('var data = (.*?);', re.DOTALL | re.MULTILINE)
 
-client = InfluxDBClient('influxdb', 8086, influx_db_user, influx_db_pass, 'tca')
+client = InfluxDBClient(
+    'influxdb', 8086, influx_db_user, influx_db_pass, 'tca')
+
 
 def scrape_data():
-    fp = urllib.request.urlopen(scrape_url)
-    mybytes = fp.read()
+    try:
+        fp = urllib.request.urlopen(scrape_url)
+        mybytes = fp.read()
 
-    html = mybytes.decode("utf8")
-    fp.close()
+        html = mybytes.decode("utf8")
+        fp.close()
 
-    soup = BeautifulSoup(html, 'html.parser')
-    scripts = soup.find_all('script')
+        soup = BeautifulSoup(html, 'html.parser')
+        scripts = soup.find_all('script')
 
-    data = scrape_regex.search(str(scripts[2].string))
+        data = scrape_regex.search(str(scripts[2].string))
 
-    if data:
-        save_data(data.group(1))
+        if data:
+            save_data(data.group(1))
 
-    threading.Timer(15, scrape_data).start()
+        threading.Timer(15, scrape_data).start()
+    except Exception:
+        pass
 
 
 def save_data(raw_data):
     data = ast.literal_eval(raw_data)
 
-    gyms = {'The Mothership':'BRI', 'The Church': 'UNC', 'The Newsroom': 'GLA', 'The Prop Store' : 'PST'};
+    gyms = {'The Mothership': 'BRI', 'The Church': 'UNC',
+            'The Newsroom': 'GLA', 'The Prop Store': 'PST'}
 
     for gym in gyms.keys():
         gym_code = gyms[gym]
@@ -50,9 +56,14 @@ def save_data(raw_data):
         time = data[gym_code]['lastUpdate']
         time = re.findall(r'\((.*?)\)', time)[0]
         time = datetime.datetime.strptime(time, '%I:%M %p')
-        correctDateTime = datetime.datetime.combine(datetime.date.today(), time.time())
+        correctDateTime = datetime.datetime.combine(
+            datetime.date.today(), time.time())
 
-        json_body = [
+        try:
+            count = int(data[gym_code]['count'])
+            capactiy = int(data[gym_code]['capacity'])
+
+            json_body = [
                 {
                     "measurement": 'occupancy',
                     "tags": {
@@ -60,13 +71,13 @@ def save_data(raw_data):
                     },
                     "time": correctDateTime.isoformat(),
                     "fields": {
-                        "value": data[gym_code]['count']
+                        "value": count
                     }
                 }
             ]
-        client.write_points(json_body)
+            client.write_points(json_body)
 
-        json_body = [
+            json_body = [
                 {
                     "measurement": 'capacity',
                     "tags": {
@@ -74,10 +85,14 @@ def save_data(raw_data):
                     },
                     "time": correctDateTime.isoformat(),
                     "fields": {
-                        "value": data[gym_code]['capacity']
+                        "value": capactiy
                     }
                 }
             ]
-        client.write_points(json_body)
-    
+            client.write_points(json_body)
+        except Exception as e:
+            print(data[gym_code])
+            print(e.__class__)
+
+
 scrape_data()
